187f7eedc877c29a33cc329f7b9d9ea5b3ef2e3a
Megan Jacobs <meganjacobs@live.com>
2020-04-04 11:22:03 -0700

    added user prompts and file output

--- Develop/app.js
+++ Develop/app.js
@@ -4,30 +4,126 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​
-​
+
+const employeeArr = []; 
+
+const render = require("./lib/htmlRenderer.js");
+
+//array for user input choices 
+const inputArr = [
+    {
+        type: "list",
+        message: "Select the type of employee:",
+        name: "employeeType",
+        choices: ["Intern","Engineer","Manager"]
+    },
+    {
+        type: "input",
+        message: "Enter the name of the employee",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Enter the ID of the employee",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Enter the email of the employee",
+        name: "email"
+    },
+    {
+        type: "input", 
+        message: "Enter the employee's school",
+        name: "school",
+        when: (response) => response.employeeType === 'Intern'
+    },
+    {
+        type: "input", 
+        message: "Enter the employee's GitHub username",
+        name: "github",
+        when: (response) => response.employeeType === 'Engineer'
+    },
+    {
+        type: "input", 
+        message: "Enter the employee's office number",
+        name: "officeNum",
+        when: (response) => response.employeeType === 'Manager'
+    },
+    {
+        type: "list",
+        message: "Would you like to enter another employee?",
+        name: "anotherEmployee",
+        choices: ["Yes","No"]
+    }
+  ]
+
+function prompt() {
+    inquirer
+    //use prompt array 
+    .prompt(inputArr)
+    .then(function(response) {
+        let employee = null; 
+        if(response.employeeType === "Intern") {
+            employee = new Intern(response.name,response.id,response.email,response.school); 
+        }
+        else if(response.employeeType === "Engineer") {
+            employee = new Engineer(response.name,response.id,response.email,response.github); 
+        }
+        else {
+            employee = new Manager(response.name,response.id,response.email,response.officeNum); 
+        }
+
+        employeeArr.push(employee); 
+
+        if(response.anotherEmployee === "Yes") {
+            prompt(); 
+        }
+        else {
+            try {
+                writeToFile(render(employeeArr)); 
+            } catch {
+
+            }
+            
+        }
+    }); 
+}
+
+function writeToFile(fileData) { 
+    //check if directory exists. if not, create it 
+    if (!fs.existsSync(OUTPUT_DIR)){
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    
+    fs.writeFile(outputPath,fileData,function(error) {
+        if(error) {
+            return console.log(error); 
+        }
+    }); 
+}
+
+prompt(); 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
